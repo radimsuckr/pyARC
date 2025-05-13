f95585f5b0a9fba6be142f78042a3236ac0e99a0
Radim Sückr <radim.suckr@gmail.com>
2025-05-13 22:29:58 +0200

    fix: requirements

--- setup.py
+++ setup.py
@@ -3,22 +3,29 @@ from setuptools import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
-  name='pyarc',
-  packages=['pyarc', "pyarc.data_structures", "pyarc.algorithms", "pyarc.qcba", "pyarc.utils","pyarc.qcba.data_structures"],
-  version='1.1.1',
-  description='An implementation of CBA algorithm',
-  author='Jiří Filip',
-  author_email='j.f.ilip@seznam.cz',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  url='https://github.com/jirifilip/pyARC',
-  download_url='https://github.com/jirifilip/pyARC/archive/1.0.tar.gz',
-  keywords='classification CBA association rules machine learning',
-  classifiers=[],
-  install_requires=['pandas', 'numpy', 'sklearn', "pyfim"]
+    name="pyarc",
+    packages=[
+        "pyarc",
+        "pyarc.data_structures",
+        "pyarc.algorithms",
+        "pyarc.qcba",
+        "pyarc.utils",
+        "pyarc.qcba.data_structures",
+    ],
+    version="1.1.1",
+    description="An implementation of CBA algorithm",
+    author="Jiří Filip",
+    author_email="j.f.ilip@seznam.cz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jirifilip/pyARC",
+    download_url="https://github.com/jirifilip/pyARC/archive/1.0.tar.gz",
+    keywords="classification CBA association rules machine learning",
+    classifiers=[],
+    install_requires=["pandas", "numpy", "scikit-learn", "pyfim"],
 )
